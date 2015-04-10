289d167ebd26672a7f9e9fe0ff3d25d21f5347ab
AliceWei <alicewei1003@gmail.com>
2015-04-10 19:24:28 +0800

    add correct file

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	//animate
+	$('#fullpage').fullpage({
+		anchors: ['firstPage', 'secondPage', '3rdPage', '4thPage', 'lastPage'],
+		menu: '#menu',
+		afterLoad: function(anchorLink, index){
+			//#section 1
+			if(index == 2){
+				$('#section1').find('.title').animate({
+					top: '0',
+					opacity: '1'
+				}, 800, 'easeOutExpo');				
+				$('#section1').find('.text-about').delay(250).animate({
+					top: '0',
+					opacity: '1'
+				}, 800, 'easeOutExpo');
+			}
+			//#section 2
+			if(anchorLink == '3rdPage'){
+				$('#section2').find('.title').animate({
+					top: '0',
+					opacity: '1'
+				}, 800, 'easeOutExpo');				
+				$('#section2').find('.musicions-row').delay(250).animate({
+					top: '0',
+					opacity: '1'
+				}, 800, 'easeOutExpo');
+			}
+			//#section 3
+			if(anchorLink == '4thPage'){
+				$('#section3').find('.title').animate({
+					top: '0',
+					opacity: '1'
+				}, 800, 'easeOutExpo');	
+				$('#section3').find('.timeline-panel').delay(250).animate({
+					top: '0',
+					opacity: '1'
+				}, 800, 'easeOutExpo');											
+			}
+
+			//#section 4
+			if(anchorLink == 'lastPage'){
+				$('#section4').find('.title').animate({
+					top: '0',
+					opacity: '1'
+				}, 800, 'easeOutExpo');
+				$('#section4').find('.content form').delay(250).animate({
+					top: '0',
+					opacity: '1'
+				}, 800, 'easeOutExpo');				
+			}			
+		}
+	});
+});
